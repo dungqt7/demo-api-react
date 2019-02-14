dc78dbf5e462339b9d80097d89a9d437bcea033b
nguyen dung <dung.nguyen@reedonline.co.uk>
2019-02-14 17:35:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import PersonList from './PersonList';
+import axios from 'axios';
 class App extends Component {
+  state = {
+      persons: []
+  }
+  componentDidMount() {
+      axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+        const persons = res.data;
+        console.log(persons);
+        this.setState({persons:persons})
+     })
+              
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App"> 
+       <table>
+         <tbody>
+           {this.state.persons.map((person,i) => <PersonList key = {i} data = {person} />)}
+         </tbody>
+       </table>
       </div>
     );
   }
